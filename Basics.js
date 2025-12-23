59559d57e0dc25b534e4b3cd0a8724d6ba6c5a8b
HemLadani <hemladani4331@gmail.com>
2025-12-23 12:56:46 +0530

    Initial commit

---
+++ Basics.js
@@ -0,0 +1,80 @@
+/*var a= 4;
+console.log(a);
+var a= 5;
+console.log(a);
+
+let c="p";
+c= 8;
+console.log(c);
+
+const x= "Hem";
+console.log(x);*/
+//x=om ;Throws error as const cannot be changed
+
+/*function cal(a,b){
+    return{
+        multi: a*b,
+        sum: a+b,
+        sub: a-b
+};
+}
+
+ let total = cal(6,5);
+ console.log("the multi is" ,total.multi);
+ console.log("the sum is" ,total.sum);
+ console.log("the sub is" ,total.sub);*/
+
+ /*const sum = (a,b) => a+b ;
+
+ console.log(sum(5,6));*/
+
+// let a = [1,2,3,4,5];
+
+ //let square = a.map(num => num*num);
+ //console.log(square);
+
+ // let even = a.filter(num => num%2 ===0);
+ //console.log(even);
+
+ //let sum = a.reduce((total,num) => total + num, 0);
+ //console.log(sum)
+
+ /*let child = {
+age : 5,
+city : "Rajkot",
+ };
+child.age = 6;
+ console.log(child.age);*/
+
+/* let a = [1,5,3,4,6,7,2];
+let largest=a[0];
+for (let i=0 ; i<=a.length; i++){
+
+    if (largest < i)
+        largest = i;
+}
+console.log("Largest num is:",largest);*/
+
+//let a = [12,5,8,130,44,9,3];
+//let num = a.filter(num => num>10);
+//console.log(num);
+//let double = a.map(num => num*2);
+//console.log(double);
+//let total = a.reduce((total,num) => total+num,0);
+//console.log(total);
+
+/*let student = {
+    name: "Hem",
+    role: "Trainee",
+    skill: "JS",
+    age: 20
+};
+student.city = "rajkot";
+student.hobby = "cricket";
+student.rollno = 65;
+
+student.role = "Developer";
+ console.log(student);*/
+
+ const d = new Date("March 31, 2004 12:00:00" );
+ console.log(d);
